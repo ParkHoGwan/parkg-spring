80a07e401621150d7dee057c28941ccd78df9120
ParkHoGwan <48813042+ParkHoGwan@users.noreply.github.com>
2019-04-05 09:48:25 +0900

    second commit

--- src/main/java/org/parkg/jdbc/spring/SpringMain.java
+++ src/main/java/org/parkg/jdbc/spring/SpringMain.java
@@ -1,4 +1,4 @@
-package com.parkg.jdbc.spring;
+package org.parkg.jdbc.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
